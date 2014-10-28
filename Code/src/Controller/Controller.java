edffb15efbc5cc7e44374c6ac84e1292d0307c3f
seza443 <github.tanguy.vaessen@hotmail.com>
2014-10-28 21:41:20 +0100

    add csv file + csv lib
    Va-t-on vraiment utiliser cette librairie ? :)

--- Code/src/Controller/Controller.java
+++ Code/src/Controller/Controller.java
@@ -1,9 +1,31 @@
 package Controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Project 3");
+		try {
+			Reader reader = new FileReader("persons.csv");
+
+			CSVReader<Person> csvPersonReader = ...;
+
+			// read all entries at once
+			List<Person> persons = csvPersonReader.readAll();
+
+			// read each entry individually
+			Iterator<Person> it = csvPersonReader.iterator();
+			while (it.hasNext()) {
+			  Person p = it.next();
+			  // ...
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
