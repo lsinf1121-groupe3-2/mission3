4474a4e421692db3992adf46ceae8f4b4a321964
Kagelmacher <alexandre.hauet@gmail.com>
2014-10-30 16:39:08 +0100

    remove print

--- Code/src/dictionary/JournalsDictionary.java
+++ Code/src/dictionary/JournalsDictionary.java
@@ -8,7 +8,6 @@ import business.Journal;
 public class JournalsDictionary extends HashMap<String, Journal>{
 	
 	public JournalsDictionary(){
-		//On hésitait à mettre super(20000) car on conaissait la taille du fichier utilisé
 		super();
 	}
 }
